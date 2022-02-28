87bdbfdf1514f8deade69c4c97d96e70c96018d7
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-02-28 22:02:13 +0900

    [Finish] Chapter04

---
+++ src/Chater04.java
@@ -0,0 +1,13 @@
+
+public class Chater04 {
+	public static void main(String[] args) {
+		String name = "mont-blanc";
+		System.out.println(name);
+
+		name = "all for one. one for all.";
+		System.out.println(name);
+
+		int num = 8;
+		System.out.println(num);
+	}
+}
---
+++ src/Variable02.java
@@ -0,0 +1,10 @@
+
+public class Variable02 {
+	public static void main(String[] args) {
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+		
+		//再代入はエラーになるため、コメントアウト済み
+		//MIN_NUMBER = 20;
+	}
+}
