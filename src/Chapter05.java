037bc76bd79f55fb84cadb1658da7b4168c79b7a
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-03-01 22:04:31 +0900

    [Finish] Chapter05

---
+++ src/Chapter05.java
@@ -0,0 +1,19 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 10000L;
+		System.out.println(long1);
+		double double1 = 10000.1;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		Float float1 = Float.valueOf(0.1f);
+		System.out.println(float1);
+		Integer integer1 = Integer.valueOf(8);
+		System.out.println(integer1);
+		Character character1 = Character.valueOf('a');
+		System.out.println(character1);
+		int[] array1 = new int[] { 1, 2, 3, 4, 5 };
+		System.out.println(array1[3]);
+	}
+}
