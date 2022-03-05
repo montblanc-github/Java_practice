d8713264d309c929081caefb80a8892495a3da86
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-03-05 14:29:33 +0900

    [Finish] Chapter07

---
+++ src/Chapter07_1.java
@@ -0,0 +1,15 @@
+
+public class Chapter07_1 {
+	public static void main(String[] args) {
+		int distance = 12;
+		if (distance <= 5) {
+			System.out.println("とても近いです");
+		} else if (distance <= 10) {
+			System.out.println("近いです");
+		} else if (distance <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+}
