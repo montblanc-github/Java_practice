460a97b86c33be09a11ddbc5c1954b1a3ad9d048
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-03-09 15:41:59 +0900

    [Finish] Chapter12

---
+++ src/practice/CapsuleMan.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class CapsuleMan {
+    public String name;
+    public int age;
+
+    public CapsuleMan(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
